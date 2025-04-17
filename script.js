f7a41c4f06ad796517924115ac8633425b4cf7be
pavan8050171 <pavankumar809081@gmail.com>
2025-04-17 11:01:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,44 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { exec } = require('child_process');
-const app = express();
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  distDir: 'out',
+  images: {
+    unoptimized: true,
+    domains: [
+      "source.unsplash.com",
+      "images.unsplash.com",
+      "ext.same-assets.com",
+      "ugc.same-assets.com",
+    ],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "source.unsplash.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "ext.same-assets.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "ugc.same-assets.com",
+        pathname: "/**",
+      },
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
-// Middleware to parse JSON requests
-app.use(bodyParser.json());
-
-// Endpoint to compile C code
-app.post('/compile', (req, res) => {
-    const { code } = req.body;
-
-    if (!code) {
-        return res.status(400).send({ error: 'No code provided' });
-    }
-
-    // Save code to a temporary file
-    const fs = require('fs');
-    const filePath = './temp_code.c';
-
-    fs.writeFileSync(filePath, code, 'utf8');
-
-    // Compile the C code using gcc
-    exec(`gcc ${filePath} -o output && ./output`, (error, stdout, stderr) => {
-        if (error) {
-            return res.status(500).send({ error: stderr || error.message });
-        }
-        res.send({ result: stdout });
-    });
-});
-
-// Start the server
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
-});
+module.exports = nextConfig;
